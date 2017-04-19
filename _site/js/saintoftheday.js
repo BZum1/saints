594c0182658d6d99fb2c54c0a21872ab85a8d919
BZum1 <ben@fiatinsight.com>
2017-04-19 09:39:54 -0400

    home view with image

--- _site/js/saintoftheday.js
+++ _site/js/saintoftheday.js
@@ -1,4 +1,12 @@
+function setWindowHeight(){
+    var windowHeight = window.innerHeight;
+    var saintWrapper = document.getElementById('saintWrapper');
+    saintWrapper.style.height = windowHeight + "px";
+}
+window.addEventListener("resize",setWindowHeight,false);
+
 $(document).ready(function() {
+  setWindowHeight();
 	$.ajax({
 		type: "GET",
 		url: 'https://winereview.wpengine.com/wp-json/wp/v2/posts?per_page=1&filter[monthnum]=4&filter[day]=19',
@@ -10,44 +18,15 @@ $(document).ready(function() {
 
 			data.forEach(function(post) {
 
-        var newBottle = '';
-				var corkFull = '';
-				var i, corks, corkNo, corkYes, style;
+        var saintBox = '';
 
-				style = post.acf.style;
+				saintBox += '<div class="saintInner">';
+        saintBox += '<h1>' + post.title.rendered + '</h1>';
+        saintBox += '<p>' + post.acf.month + ' ' + post.acf.day + ' <span>Saint of the Day</span></p>';
+				saintBox += '<div class="description">' + post.content.rendered + '</div>';
+        saintBox += '</div>';
 
-				var month = post.acf.month;
-				var day = post.acf.day;
-
-				switch (style) {
-
-					case 'red':
-						styleBlock = '<div class="bottle color-red">';
-						break;
-
-					case 'white':
-						styleBlock = '<div class="bottle color-white">';
-						break;
-
-					case 'rose':
-						styleBlock = '<div class="bottle color-rose">';
-						break;
-
-					default:
-						styleBlock = '';
-						break;
-				}
-
-				newBottle += '<div class="col-sm-4 bottle-wrap my-3">';
-
-				newBottle += styleBlock;
-        newBottle += '<h3>' + post.title.rendered + '</h3>';
-        newBottle += '<p>' + post.acf.month + ' ' + post.acf.day + '</p>';
-				newBottle += '<div class="bottle-description">' + post.content.rendered + '</div>';
-
-        newBottle += '</div></div>';
-
-        document.getElementById('saintToday').innerHTML += newBottle;
+        document.getElementById('saintToday').innerHTML += saintBox;
 
 			});
 		}
